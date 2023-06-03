73c782bbd7c9dc231ca256aa901aaa4b36a8cc99
unknown <carlossaid456@gmail.com>
2023-06-03 16:43:49 -0600

    Contexto global

--- src/Components/Card/indes.jsx
+++ src/Components/Card/indes.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react"
+import { ShoppingCartContext }  from "../../Context"
+
 function Card(data) {
+  const context = useContext(ShoppingCartContext)
   return (
     <div className="drop-shadow-2xl bg-white cursor-pointer w-56 h-60  rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
@@ -10,7 +14,9 @@ function Card(data) {
           src={data.data.image}
           alt={data.data.title}
         />
-        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
+        <div 
+          className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+          onClick={() => context.setCount(context.count + 1)}>
           +
         </div>
       </figure>
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartProvider }  from '../../Context'
 import './App.css'
 import { Home } from '../Home'
 import { MyAccount } from '../MyAccount'
@@ -24,10 +25,12 @@ const AppRoutes = () => {
 
 function App() {
   return (
+    <ShoppingCartProvider>
     <BrowserRouter >
       < AppRoutes />
       < Navbar />
     </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
