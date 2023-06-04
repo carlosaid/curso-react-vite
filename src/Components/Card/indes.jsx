9f92692bb1cc3479a9f6b292045edc4a3846abdc
unknown <carlossaid456@gmail.com>
2023-06-03 20:52:05 -0600

    Actualizaion Card

--- src/Components/Card/indes.jsx
+++ src/Components/Card/indes.jsx
@@ -1,18 +1,18 @@
 import { useContext } from "react"
 import { ShoppingCartContext }  from "../../Context"
 
-function Card(data) {
+function Card({ data }) {
   const context = useContext(ShoppingCartContext)
   return (
     <div className="drop-shadow-2xl bg-white cursor-pointer w-56 h-60  rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          {data.data.category}
+          {data.category}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src={data.data.image}
-          alt={data.data.title}
+          src={data.image}
+          alt={data.title}
         />
         <div 
           className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
@@ -21,8 +21,8 @@ function Card(data) {
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light p-2">{data.data.title}</span>
-        <span className="text-lg font-medium p-2">${data.data.price}</span>
+        <span className="text-sm font-light p-2">{data.title}</span>
+        <span className="text-lg font-medium p-2">${data.price}</span>
       </p>
     </div>
   );
